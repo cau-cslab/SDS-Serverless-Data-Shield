37b315d92abf3756baf63688ba3160c975bd5c5d
Gyupil <mamonde1015@gmail.com>
2025-08-04 20:59:53 +0900

    feat: key creation, reset method, and auto-using decorator

---
+++ SDS/cryptoHandler.py
@@ -0,0 +1,21 @@
+import os
+from functools import wraps
+from memtools import MemView
+
+class CryptoHandler:
+    ephemeralKey = None
+
+    @staticmethod
+    def changeKey():
+        CryptoHandler.ephemeralKey = MemView(int.from_bytes(os.urandom(32), byteorder='big'))
+
+    @staticmethod
+    def useKey(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(CryptoHandler.ephemeralKey, *args, **kwargs)
+        return wrapper
+
+    @staticmethod
+    def setZero():
+        CryptoHandler.ephemeralKey.clear()
