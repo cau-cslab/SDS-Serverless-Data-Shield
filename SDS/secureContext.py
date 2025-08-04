542e4a277dc5dab41c9246734a2714e81ce1967f
Gyupil <mamonde1015@gmail.com>
2025-08-04 20:59:23 +0900

    feat: the overall structure of SecureContext context manager

---
+++ SDS/secureContext.py
@@ -0,0 +1,13 @@
+from cryptoHandler import CryptoHandler
+
+class SecureContext:
+    deleteTarget = []
+    def __enter__(self):
+        CryptoHandler.changeKey()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for i in range(len(SecureContext.deleteTarget)):
+            SecureContext.deleteTarget[i].clear()
+            del SecureContext.deleteTarget[i]
+        SecureContext.deleteTarget.clear()
+        CryptoHandler.setZero()
